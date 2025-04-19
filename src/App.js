b757f4fefe1e0a0371266957da378fd78772fae8
mint723 <neu990112@gmail.com>
2025-04-19 14:28:34 +0900

    fix: add path

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./layout/Header/Header";
 import Footer from "./layout/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import Login from "./pages/Login/Login";
+import List from "./pages/resume/List";
 
 const PageLayout = (children) => {
   return (
@@ -24,6 +25,9 @@ function App() {
           <ScrollToTop/>
           <Routes>
             <Route path='/login' element={<Login/>}></Route>
+          </Routes>
+          <Routes>
+            <Route path='/resume/list' element={<List/>}></Route>
           </Routes>
         </Router>
         <Footer/>
